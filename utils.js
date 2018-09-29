14731ce1d6b7d62c8cf6c22c39a9a0ee087219c6
Hzy0913 <758858816@qq.com>
2018-09-29 19:01:04 +0800

    feat(utils):  update quickSort

--- utils.js
+++ utils.js
@@ -144,3 +144,22 @@ function bubbleSort(data) {
   }
   return data;
 }
+
+/**
+ * 快速排序(从小到大)
+ */
+function quickSort(arr) {
+  if (arr.length <= 1) { return arr; }
+  const pivotIndex = Math.floor(arr.length / 2);
+  const pivot = arr.splice(pivotIndex, 1)[0];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat([pivot], quickSort(right));
+};

8149af54a4ae1051548477d040f3d9754b3a8558
Hzy0913 <758858816@qq.com>
2019-02-16 19:15:11 +0800

    feat(utils):  update throttle func

--- utils.js
+++ utils.js
@@ -160,3 +160,18 @@ function quickSort(arr) {
   }
   return quickSort(left).concat([pivot], quickSort(right));
 };
+
+/**
+ * 函数截流
+ */
+function throttle(fn, delay) {
+  let _lastTime = null;
+  return function () {
+    const context = this, args = arguments;
+    let _nowTime = + new Date();
+    if (_nowTime - _lastTime > delay || !_lastTime) {
+      fn.apply(context, args);
+      _lastTime = _nowTime;
+    }
+  }
+};
